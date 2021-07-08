0d11576ee22601f648a1d5d3aa8c82e3a4a86477
RobertUriarte <robbyuriarte2@gmail.com>
2021-07-08 19:44:09 -0400

    List and Item class created

--- src/main/java/ucf/assignments/Todo_Item.java
+++ src/main/java/ucf/assignments/Todo_Item.java
@@ -9,12 +9,30 @@ public class Todo_Item {
     //Set description
     //Set due date
     //Set status
+    private String name;
+    private String description;
+    private String due_date;
+    private String status;
 
     //constructor for item
-
+    public Todo_Item(String name, String description, String due_date, String status) {
+        this.name = name;
+        this.description = description;
+        this.due_date = due_date;
+        this.status = status;
+    }
 
     //getter for item
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public String getDue_date() { return due_date; }
+    public String getStatus() { return status; }
+
 
     //setter for item
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setDue_date(String due_date) { this.due_date = due_date; }
+    public void setStatus(String status) { this.status = status; }
 
 }
--- src/main/java/ucf/assignments/Todo_List.java
+++ src/main/java/ucf/assignments/Todo_List.java
@@ -4,14 +4,25 @@
  */
 package ucf.assignments;
 
+import java.util.List;
+
 public class Todo_List {
     //Set name
     //Set List of Items
+    private String name;
+    private List<Todo_Item> ItemList;
 
     //constructor for list
-
+    public Todo_List(String name, List<Todo_Item> ItemList) {
+        this.name = name;
+        this.ItemList = ItemList;
+    }
 
     //getter for list
+    public String getName() { return name; }
+    public List<Todo_Item> getItemList() { return ItemList; }
 
     //setter for list
+    public void setName(String name) { this.name = name; }
+    public void setItemList(List<Todo_Item> ItemList) { this.ItemList = ItemList; }
 }
