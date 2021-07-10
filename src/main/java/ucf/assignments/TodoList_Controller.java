0f6735c0009284216d8e6648c7cf6866e8ec35a7
RobertUriarte <robbyuriarte2@gmail.com>
2021-07-09 20:35:25 -0400

    Todo List can be completely edited besides due date. Gui can now display completed and uncompleted list

--- src/main/java/ucf/assignments/TodoList_Controller.java
+++ src/main/java/ucf/assignments/TodoList_Controller.java
@@ -4,82 +4,75 @@
  */
 package ucf.assignments;
 
-import com.sun.tools.javac.Main;
-import javafx.beans.property.SimpleStringProperty;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.ResourceBundle;
+
+import java.util.*;
 
 public class TodoList_Controller implements Initializable {
-    @FXML private TableView<Todo_Item> tableView;
-    @FXML private TableColumn<Todo_Item, String> title;
-    @FXML private TableColumn<Todo_Item, String> description;
-    @FXML private TableColumn<Todo_Item, LocalDate> due_date;
+    @FXML
+    private TableView<Todo_Item> tableView;
+    @FXML
+    private TableColumn<Todo_Item, String> title;
+    @FXML
+    private TableColumn<Todo_Item, String> description;
+    @FXML
+    private TableColumn<Todo_Item, LocalDate> due_date;
+    @FXML
+    private TableColumn<Todo_Item, String> status;
+
     //Initialize Empty List<Todo_list>
+
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private TextField descriptionTextField;
+    @FXML
+    private DatePicker dueDatePicker;
+    @FXML
+    private TextField statusTextField;
+
+    public ObservableList<Todo_Item> item_list = FXCollections.observableArrayList();
 
     //Initialize file chooser
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //Set file chooser to a specific directory
-        title.setCellValueFactory(new PropertyValueFactory<Todo_Item,String>("title"));
-        description.setCellValueFactory(new PropertyValueFactory<Todo_Item,String>("description"));
-        due_date.setCellValueFactory(new PropertyValueFactory<Todo_Item,LocalDate>("due_date"));
+        title.setCellValueFactory(new PropertyValueFactory<>("title"));
+        description.setCellValueFactory(new PropertyValueFactory<>("description"));
+        due_date.setCellValueFactory(new PropertyValueFactory<>("due_date"));
+        status.setCellValueFactory(new PropertyValueFactory<>("status"));
 
-        tableView.setItems(getSample());
+        tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        tableView.setItems(item_list);
 
         tableView.setEditable(true);
+
         title.setCellFactory(TextFieldTableCell.forTableColumn());
         description.setCellFactory(TextFieldTableCell.forTableColumn());
+        status.setCellFactory(TextFieldTableCell.forTableColumn());
+
     }
 
-    public ObservableList<Todo_Item> getSample(){
-        ObservableList<Todo_Item> new_item = FXCollections.observableArrayList();
-        new_item.add(new Todo_Item("Dogs","Buy dog food",LocalDate.of(2021,Month.JANUARY,1)));
-        return new_item;
-    }
-
-    @FXML
-    public void FileSaveClicked(ActionEvent actionEvent) {
-        //Initialize a file to be saved
-        //Get all the list to save all the items from our Existing List<Todo_list>
-        //Call FilePrint(file,all the list) function
-    }
-
-    public void FilePrint(File file, List<Todo_List> list){
+    public void FilePrint(File file, List<Todo_List> list) {
         //Use print writer to print list data to our new file
         //Close file
     }
 
-    @FXML
-    public void FileLoadClicked(ActionEvent actionEvent) {
-        //Initialize a file to the file selected by the user
-        //Call ParseData() function to get the data from the file selected
-        //Display newly updated List(s)
-    }
 
-    public void ParseData(File file){
+    public void ParseData(File file) {
         //Takes in a file, preferably a text file
         //Initialize a scanner
         //Scan line by line and scan for list and items within a list
@@ -87,61 +80,9 @@ public class TodoList_Controller implements Initializable {
     }
 
     @FXML
-    public void ListSelectedMouse(MouseEvent arg0){
-        //Store list name in string
-        //Get index of selected list
-        //Set the text bar to list name selected
-        //return value of text bar
-    }
-
-    @FXML
-    public void ViewButtonClicked(ActionEvent actionEvent) throws IOException {
-        //Get the index of the list selected
-        //Create a new parent for the ItemsView Scene
-        //Create a new scene for ItemsView scene
-        //Get the current stage we are on
-        //Set the current stage to the ItemsView scene
-        //Set values of Table view to the selected list's values
-    }
-
-    @FXML
-    public void EditButtonClicked(ActionEvent actionEvent) {
-        //Get the text in the text bar/User input for the new name
-        //Set the name of the current list with the new name
-        //Display updated values
-
-    }
-
-    @FXML
-    public void AddButtonClicked(ActionEvent actionEvent) {
-        //Get the text in the text bar
-        //Create a new empty list with name in text bar
-        //Add new list to List of lists
-        //Display updated List of lists
-
-    }
-
-    @FXML
-    public void RemoveButtonClicked(ActionEvent actionEvent) {
-        //Remove the selected list using the index
-    }
-
-    @FXML
-    public void ItemSelectedMouse(ActionEvent actionEvent){
-        //Get index of the item selected
-    }
-
-    @FXML
-    public void BackButtonPushed(ActionEvent actionEvent) throws IOException {
-        //Create a new parent for the Main Scene
-        //Create a new scene for Main scene
-        //Get the current stage we are on
-        //Set the current stage to the Main scene
-    }
-
-    @FXML
     public void DisplayAllButtonClicked(ActionEvent actionEvent) {
         //Set the tableview to display all items in the List of items of current List
+        tableView.setItems(item_list);
     }
 
     @FXML
@@ -149,6 +90,17 @@ public class TodoList_Controller implements Initializable {
         //Initialize new list
         //Copy over the items that don't have their status as "complete" into our new list
         //Set the tableview values to the newly created Incomplete items list
+
+        FilteredList<Todo_Item> items = new FilteredList<>(item_list,b -> true);
+        items.setPredicate(Todo_Item -> {
+            if(Todo_Item.getStatus().equals("I") || Todo_Item.getStatus().equals("i")){
+                return true;
+            }
+            else{
+                return false;
+            }
+        });
+        tableView.setItems(items);
     }
 
     @FXML
@@ -156,6 +108,16 @@ public class TodoList_Controller implements Initializable {
         //Initialize new list
         //Copy over the items that have their status as "complete" into our new list
         //Set the tableview values to the newly created Incomplete items list
+        FilteredList<Todo_Item> items = new FilteredList<>(item_list,b -> true);
+        items.setPredicate(Todo_Item -> {
+            if(Todo_Item.getStatus().equals("C") || Todo_Item.getStatus().equals("c")){
+                return true;
+            }
+            else{
+                return false;
+            }
+        });
+        tableView.setItems(items);
     }
 
     @FXML
@@ -168,6 +130,8 @@ public class TodoList_Controller implements Initializable {
         //Get the current stage we are on
         //Set the current stage to the AddNewItem scene
 
+        Todo_Item new_item = new Todo_Item(titleTextField.getText(), descriptionTextField.getText(), dueDatePicker.getValue(), statusTextField.getText());
+        tableView.getItems().add(new_item);
     }
 
     @FXML
@@ -175,6 +139,21 @@ public class TodoList_Controller implements Initializable {
         //Gets the index of the item
         //Removes that item from item list
         //Display new tableview with removed item
+
+        ObservableList<Todo_Item> selectedRows, allItems;
+        allItems = tableView.getItems();
+        selectedRows = tableView.getSelectionModel().getSelectedItems();
+        if(selectedRows != null)
+            allItems.removeAll(selectedRows);
+
+    }
+
+    @FXML
+    public void ClearAllButtonClicked(ActionEvent actionEvent) {
+        ObservableList<Todo_Item> allItems, all;
+        allItems = tableView.getItems();
+        all = tableView.getItems();
+        allItems.removeAll(all);
     }
 
     @FXML
@@ -184,28 +163,9 @@ public class TodoList_Controller implements Initializable {
         //Call FilePrint() function
     }
 
-    @FXML
-    public void CancelButtonClicked(ActionEvent actionEvent) throws IOException {
-        //Create a new parent for the ItemsView Scene
-        //Create a new scene for ItemsView scene
-        //Get the current stage we are on
-        //Set the current stage to the ItemsView scene
-
-    }
 
     @FXML
-    public void FinishButtonClicked(ActionEvent actionEvent) {
-        //Get the values currently in the tableview
-        //Create a new item and inserts current values of table view
-        //Adds new item to current list
-        //Create a new parent for the ItemsView Scene
-        //Create a new scene for ItemsView scene
-        //Get the current stage we are on
-        //Set the current stage to the ItemsView scene
-    }
-
-    @FXML
-    public void ChangeTitleCell(TableColumn.CellEditEvent newCell){
+    public void ChangeTitleCell(TableColumn.CellEditEvent newCell) {
         //Get the selected cell
         //Set cell to editable
         //Get user input from cell value
@@ -217,7 +177,7 @@ public class TodoList_Controller implements Initializable {
     }
 
     @FXML
-    public void ChangeDescriptionCell(TableColumn.CellEditEvent newCell){
+    public void ChangeDescriptionCell(TableColumn.CellEditEvent newCell) {
         //Get the selected cell
         //Set cell to editable
         //Get user input from cell value
@@ -225,11 +185,20 @@ public class TodoList_Controller implements Initializable {
         //Update the Description string value of our item
 
         Todo_Item item_selected = tableView.getSelectionModel().getSelectedItem();
-        item_selected.setDescription(newCell.getNewValue().toString());
+        String user_input = newCell.getNewValue().toString();
+        if(user_input.length() < 1){
+        }
+        else if(user_input.length() > 256){
+            String cutString = user_input.substring(1, 256);
+            item_selected.setDescription(cutString);
+        }
+        else{
+            item_selected.setDescription(user_input);
+        }
     }
 
     @FXML
-    public void ChangeDueDateCell(){
+    public void ChangeDueDateCell(TableColumn.CellEditEvent newCell) {
         //Get the selected cell
         //Set cell to editable
         //Get user input from cell value
@@ -238,13 +207,22 @@ public class TodoList_Controller implements Initializable {
         //If input is not in correct format, input is changed to correct format
         //Update the Due Date string value of our item
         //Note: Date picker in Scene builder could be used here as alternative
+
+        Todo_Item item_selected = tableView.getSelectionModel().getSelectedItem();
+        item_selected.setDue_date((LocalDate) newCell.getNewValue());
+
     }
+
     @FXML
-    public void ChangeStatusCell(){
+    public void ChangeStatusCell(TableColumn.CellEditEvent newCell) {
         //Get the selected cell
         //Set cell to editable
         //Get user input from cell value
         //Set the status cell to new user input
         //Update the Status string value of our item
+
+        Todo_Item item_selected = tableView.getSelectionModel().getSelectedItem();
+        item_selected.setStatus(newCell.getNewValue().toString());
     }
 }
+
--- src/main/java/ucf/assignments/Todo_Item.java
+++ src/main/java/ucf/assignments/Todo_Item.java
@@ -17,18 +17,21 @@ public class Todo_Item {
     private SimpleStringProperty title;
     private SimpleStringProperty description;
     private LocalDate due_date;
+    private SimpleStringProperty status;
 
     //constructor for item
-    public Todo_Item(String title, String description, LocalDate due_date) {
+    public Todo_Item(String title, String description, LocalDate due_date, String status) {
         this.title = new SimpleStringProperty(title);
         this.description = new SimpleStringProperty(description);
         this.due_date = due_date;
+        this.status = new SimpleStringProperty(status);
     }
 
     //getter for item
     public String getTitle() { return title.get(); }
     public String getDescription() { return description.get(); }
     public LocalDate getDue_date() { return due_date; }
+    public String getStatus() { return status.get(); }
 
 
 
@@ -36,6 +39,7 @@ public class Todo_Item {
     public void setTitle(String title) { this.title = new SimpleStringProperty(title); }
     public void setDescription(String description) { this.description = new SimpleStringProperty(description); }
     public void setDue_date(LocalDate due_date) { this.due_date = due_date; }
+    public void setStatus(String status) { this.status = new SimpleStringProperty(status); }
 
 
 }
